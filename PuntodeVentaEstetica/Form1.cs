0b25b996cb52c5006745aa1408aecddd4cb81f1e
Francisco-Freyre <freyrefrancisco.v@gmail.com>
2020-06-26 20:50:55 -0500

    Interfaz grafica al 50%

--- PuntodeVentaEstetica/Form1.cs
+++ PuntodeVentaEstetica/Form1.cs
@@ -16,5 +16,57 @@ namespace PuntodeVentaEstetica
         {
             InitializeComponent();
         }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnRestaurar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            btnMaximizar.Visible = true;
+            btnRestaurar.Visible = false;
+        }
+
+        private void contenido_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnMaximizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+            btnMaximizar.Visible = false;
+            btnRestaurar.Visible = true;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bunifuGradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            if(bunifuGradientPanel1.Width == 225)
+            {
+                bunifuGradientPanel1.Visible = false;
+                bunifuGradientPanel1.Width = 68;
+                SideBar.Width = 90;
+                AnimationSidebar.Show(bunifuGradientPanel1);
+            }
+            else
+            {
+                bunifuGradientPanel1.Visible = false;
+                bunifuGradientPanel1.Width = 225;
+                SideBar.Width = 250;
+                AnimationSideBarBack.Show(bunifuGradientPanel1);
+            }
+        }
     }
 }
